84ab5b787e636a6ee79dad9ed415f4e95e120948
YuChen Chai <5936773944@qq.com>
2019-06-24 11:43:32 +0800

    Add more charts

--- js/Function_Calculate.js
+++ js/Function_Calculate.js
@@ -148,3 +148,325 @@ calculate=function(section)
     }
 
 }
+var open_cover_img = function(url){
+    // $("#cover_img").attr("src",url);
+    // $("#cover_img").attr("width","50%");
+    // $("#cover_img").attr("height","50%");
+    // $("#cover_img").attr("src",url);
+
+
+    var dom = document.getElementById("show_container");
+
+    selectValue=$("#MSelect").val();
+
+    var myChart = echarts.init(dom);
+    var app = {};
+    var ROOT_PATH=window.location.host;
+    var Icons = {
+        'Car': ROOT_PATH+'resource/automobile.png',
+        'Bus': ROOT_PATH+'resource/buses.png',
+        'Bike': ROOT_PATH+'resource/bicycle.png',
+        'Walk': ROOT_PATH+'resource/walker.png'
+    };
+    option = null;
+    if(selectValue==0)
+    {
+        $('#chart_title').text("出行方式对比：时间、距离");
+        var seriesLabel = {
+            normal: {
+                show: true,
+                textBorderColor: '#333',
+                textBorderWidth: 2
+            }
+        }
+
+        option = {
+            // title: {
+            //     text: '出行方式：时间、距离对比'
+            // },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            legend: {
+                data: ['距离', '时间']
+            },
+            grid: {
+                left: 100
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'value',
+                name: '距离, 时间',
+                axisLabel: {
+                    formatter: '{value}'
+                }
+            },
+            yAxis: {
+                type: 'category',
+                inverse: true,
+                data: ['Car', 'Bus','Bike','Walk'],
+                axisLabel: {
+                    formatter: function (value) {
+                        return '{' + value + '| }\n{value|' + value + '}';
+                    },
+                    margin: 20,
+                    rich: {
+                        value: {
+                            lineHeight: 30,
+                            align: 'center'
+                        },
+                        Car: {
+                            height: 40,
+                            align: 'center',
+                            backgroundColor: {
+                                image: Icons.Car
+                            }
+                        },
+                        Bus: {
+                            height: 40,
+                            align: 'center',
+                            backgroundColor: {
+                                image: Icons.Bus
+                            }
+                        },
+                        Bike: {
+                            height: 40,
+                            align: 'center',
+                            backgroundColor: {
+                                image: Icons.Bike
+                            }
+                        },
+                        Walk: {
+                            height: 40,
+                            align: 'center',
+                            backgroundColor: {
+                                image: Icons.Walk
+                            }
+                        }
+                    }
+                }
+            },
+            series: [
+                {
+                    name: '距离',
+                    type: 'bar',
+                    data: [7,5,12,15],
+                    label: seriesLabel,
+                    markPoint: {
+                        symbolSize: 1,
+                        symbolOffset: [0, '50%'],
+                        label: {
+                            normal: {
+                                formatter: '{a|{a}\n}{b|{b} }{c|{c}}',
+                                backgroundColor: 'rgb(242,242,242)',
+                                borderColor: '#aaa',
+                                borderWidth: 1,
+                                borderRadius: 4,
+                                padding: [4, 10],
+                                lineHeight: 26,
+                                // shadowBlur: 5,
+                                // shadowColor: '#000',
+                                // shadowOffsetX: 0,
+                                // shadowOffsetY: 1,
+                                position: 'right',
+                                distance: 20,
+                                rich: {
+                                    a: {
+                                        align: 'center',
+                                        color: '#fff',
+                                        fontSize: 18,
+                                        textShadowBlur: 2,
+                                        textShadowColor: '#000',
+                                        textShadowOffsetX: 0,
+                                        textShadowOffsetY: 1,
+                                        textBorderColor: '#333',
+                                        textBorderWidth: 2
+                                    },
+                                    b: {
+                                        color: '#333'
+                                    },
+                                    c: {
+                                        color: '#ff8811',
+                                        textBorderColor: '#000',
+                                        textBorderWidth: 1,
+                                        fontSize: 22
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                {
+                    name: '时间',
+                    type: 'bar',
+                    label: seriesLabel,
+                    data: [35, 44, 65,120]
+                }
+            ]
+        };
+        ;
+        if (option && typeof option === "object") {
+            myChart.setOption(option, true);
+        }
+    }
+    else if(selectValue==1)
+    {
+        var seriesLabel = {
+            normal: {
+                show: true,
+                textBorderColor: '#333',
+                textBorderWidth: 2
+            }
+        }
+
+        option = {
+            // title: {
+            //     text: '出行方式：时间、距离对比'
+            // },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            legend: {
+                data: ['距离', '时间']
+            },
+            grid: {
+                left: 100
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'value',
+                name: 'Days',
+                axisLabel: {
+                    formatter: '{value}'
+                }
+            },
+            yAxis: {
+                type: 'category',
+                inverse: true,
+                data: ['Car', 'Bus','Bike','Walk'],
+                axisLabel: {
+                    formatter: function (value) {
+                        return '{' + value + '| }\n{value|' + value + '}';
+                    },
+                    margin: 20,
+                    rich: {
+                        value: {
+                            lineHeight: 30,
+                            align: 'center'
+                        },
+                        Car: {
+                            height: 40,
+                            align: 'center',
+                            backgroundColor: {
+                                image: Icons.Car
+                            }
+                        },
+                        Bus: {
+                            height: 40,
+                            align: 'center',
+                            backgroundColor: {
+                                image: Icons.Bus
+                            }
+                        },
+                        Bike: {
+                            height: 40,
+                            align: 'center',
+                            backgroundColor: {
+                                image: Icons.Bike
+                            }
+                        },
+                        Walk: {
+                            height: 40,
+                            align: 'center',
+                            backgroundColor: {
+                                image: Icons.Walk
+                            }
+                        }
+                    }
+                }
+            },
+            series: [
+                {
+                    name: '距离',
+                    type: 'bar',
+                    data: [32,35,42,45],
+                    label: seriesLabel,
+                    markPoint: {
+                        symbolSize: 1,
+                        symbolOffset: [0, '50%'],
+                        label: {
+                            normal: {
+                                formatter: '{a|{a}\n}{b|{b} }{c|{c}}',
+                                backgroundColor: 'rgb(242,242,242)',
+                                borderColor: '#aaa',
+                                borderWidth: 1,
+                                borderRadius: 4,
+                                padding: [4, 10],
+                                lineHeight: 26,
+                                // shadowBlur: 5,
+                                // shadowColor: '#000',
+                                // shadowOffsetX: 0,
+                                // shadowOffsetY: 1,
+                                position: 'right',
+                                distance: 20,
+                                rich: {
+                                    a: {
+                                        align: 'center',
+                                        color: '#fff',
+                                        fontSize: 18,
+                                        textShadowBlur: 2,
+                                        textShadowColor: '#000',
+                                        textShadowOffsetX: 0,
+                                        textShadowOffsetY: 1,
+                                        textBorderColor: '#333',
+                                        textBorderWidth: 2
+                                    },
+                                    b: {
+                                        color: '#333'
+                                    },
+                                    c: {
+                                        color: '#ff8811',
+                                        textBorderColor: '#000',
+                                        textBorderWidth: 1,
+                                        fontSize: 22
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                {
+                    name: '时间',
+                    type: 'bar',
+                    label: seriesLabel,
+                    data: [35, 44, 65,120]
+                }
+            ]
+        };
+        ;
+        if (option && typeof option === "object") {
+            myChart.setOption(option, true);
+        }
+    }
+
+    $("#enlarge").show();
+};
+var close_cover_img = function(){
+    $("#enlarge").hide();
+};
